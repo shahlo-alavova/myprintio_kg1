95bec3d0e81a823e07b1e303650d59ec6a8f1034
shahlo-alavova <64728278+shahlo-alavova@users.noreply.github.com>
2021-05-30 16:36:20 +0600

    impoved mistakes

--- src/components/Editing/Photo/dropzone2.js
+++ src/components/Editing/Photo/dropzone2.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 
 const Dropzone1 = () => {
 const [Drag, setDrag] = useState(false);
+
 function dragStartHandler(e){
     e.preventDefault();
     setDrag(true);
@@ -11,12 +12,23 @@ function dragLeaveHandler(e){
     e.preventDefault();
     setDrag(false);
 }
+
+function onDropHandler(e){
+e.preventDefault();
+let files=[...e.dataTransfer.files]
+console.log(files);
+setDrag(false);
+}
+
+
  return(
 <div>
     {Drag ? <div className="drop-area" 
     onDragOver ={e => dragStartHandler(e)}
     onDragLeave={e => dragLeaveHandler(e)}
-    onDragStart={e=> dragStartHandler(e)}>Отпустите файлы, чтобы загрузить их</div>
+    onDragStart={e=> dragStartHandler(e)}
+    onDrop={e=> onDropHandler(e)}
+    >Отпустите файлы, чтобы загрузить их</div>
     : <div
     onDragOver ={e => dragStartHandler(e)}
     onDragLeave={e => dragLeaveHandler(e)}
--- src/components/ImageTshirt/imageTshirt.js
+++ src/components/ImageTshirt/imageTshirt.js
@@ -6,8 +6,8 @@ import './../../App.css';
 function ImageTshirt(){
 return(
 <div>
-       <div className='tshirtWrap'><img action="/upload-target" class="dropzone" alt='futbolka' className="imageTshirt" src={tshirt}></img>
-      <label id='textsection' className='textTshirt'>Введите текст</label></div>
+       <div className="tshirtWrap"><img action="/upload-target" className="dropzone" alt='futbolka' className="imageTshirt" src={tshirt}/>
+      <label id='textsection' className="textTshirt">Введите текст</label></div>
       <DropzoneComponent/>
       </div>);
 }
